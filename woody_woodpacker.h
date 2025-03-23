ec23fdbb473276bf7ed818e2217dc2e25dcea9f0
Devsstudent <devsstudent777@gmail.com>
2025-03-23 17:14:21 +0100

    remove header

--- woody_woodpacker.h
+++ woody_woodpacker.h
@@ -9,25 +9,6 @@
 #include <stdlib.h>
 #include <elf.h>
 
-typedef uint64_t	Elf64_Addr;
-typedef uint16_t	Elf64_Half;
-typedef uint64_t	Elf64_Off;
-typedef int32_t		Elf64_Sword;
-typedef int64_t		Elf64_Sxword;
-typedef uint32_t	Elf64_Word;
-typedef uint64_t	Elf64_Lword;
-typedef uint64_t	Elf64_Xword;
-
-typedef struct elf64_phdr {
-    Elf64_Word p_type;
-    Elf64_Word p_flags;
-    Elf64_Off p_offset;		/* Segment file offset */
-    Elf64_Addr p_vaddr;		/* Segment virtual address */
-    Elf64_Addr p_paddr;		/* Segment physical address */
-    Elf64_Xword p_filesz;		/* Segment size in file */
-    Elf64_Xword p_memsz;		/* Segment size in memory */
-    Elf64_Xword p_align;		/* Segment alignment, file & memory */
-  } Elf64_Phdr;
 
 /*
 typedef uint64_t	Elf64_Addr;
