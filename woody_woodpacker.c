c6a63a3fe592ec7e436b0fabede944d97626e01b
Devsstudent <devsstudent777@gmail.com>
2025-03-29 18:36:19 +0100

    adding the new header before changing section offset

--- woody_woodpacker.c
+++ woody_woodpacker.c
@@ -289,17 +289,17 @@ bool    insert_new_phdr(int stream, size_t original_len, size_t added_bytes) {
         return 1;
     }
 
+    if (!insert_data(stream, offset_new_phdr, sizeof(Elf64_Phdr), (char *)program_header)) {
+        printf("Error: could not insert program header\n");
+        return 1;
+    };
+
     // Modify section header table entries
     if (!modify_entrypoints_section_headers(stream, sizeof(Elf64_Phdr), shoff + sizeof(Elf64_Phdr), shnum)) {
         printf("Error: could not modify section header entrypoints\n");
         return 1;
     };
 
-    // Read les infos des headers, pour modifier la valeur de l'entrypoint offset
-    if (!insert_data(stream, offset_new_phdr, sizeof(Elf64_Phdr), (char *)program_header)) {
-        printf("Error: could not insert program header\n");
-        return 1;
-    };
     return true;
 }
 
